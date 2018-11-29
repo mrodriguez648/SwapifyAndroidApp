8b135d933e17679ea4c35a1ebb80f17535f21733
Diego Perez <diegoperez7055@gmail.com>
2018-11-29 12:58:14 -0800

    Add names of artists in playlist and fix recyclerview width and height

--- app/src/main/java/cs184/cs/ucsb/edu/SwapifyAndroidApp/MainActivity.java
+++ app/src/main/java/cs184/cs/ucsb/edu/SwapifyAndroidApp/MainActivity.java
@@ -16,7 +16,10 @@ import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.crypto.AEADBadTagException;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
@@ -33,17 +36,19 @@ import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 1337;
-    private static final String CLIENT_ID ="49561555a6fd4897912fddebb7bf7da8";
+    private static final String CLIENT_ID = "49561555a6fd4897912fddebb7bf7da8";
     private static final String REDIRECT_URI = "testspotify://callback";
     private static ArrayList<PlaylistSimple> userPlaylists;
     private static ArrayList<PlaylistTrack> playlistSongs;
+    private static String artistString;
     SpotifyApi api;
-    static SpotifyService  spotify;
+    static SpotifyService spotify;
 
     //Recycler View Variables: Playlists
     private ArrayList<String> mPlaylistName = new ArrayList<>();
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
+    private static HashMap<String,String> PlaylistToArtists = new HashMap<>();
 
 
     @Override
@@ -66,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -78,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 // Response was successful and contains auth token
                 case TOKEN:
                     // Handle successful response
-                    Log.d("user","success");
+                    Log.d("user", "success");
                     api.setAccessToken(response.getAccessToken());
                     spotify = api.getService();
                     //set welcome
@@ -86,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void success(UserPrivate userPrivate, Response response) {
                             TextView welcome = findViewById(R.id.Welcome);
-                            welcome.setText("WELCOME TO SWAPIFY " +userPrivate.display_name);
+                            welcome.setText("WELCOME TO SWAPIFY " + userPrivate.display_name);
 
                         }
 
@@ -98,12 +102,14 @@ public class MainActivity extends AppCompatActivity {
                     //activate button
                     Button launchPlaylistViewer = findViewById(R.id.button);
                     getPlaylists();
+
                     launchPlaylistViewer.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             //LAUNCH PLAYLIST RECYCLERVIEW ACTIVITY HERE
                             //PASS THROUGH THE ARRAYLIST PLAYLIST VARAIBLE HERE
-                            Log.d("user",Integer.toString(userPlaylists.size()));
+
+                            Log.d("user", Integer.toString(userPlaylists.size()));
                             setContentView(R.layout.playlist_layout);
                             initImageBitmaps(userPlaylists, playlistSongs);
 
@@ -114,47 +120,26 @@ public class MainActivity extends AppCompatActivity {
                 // Auth flow returned an error
                 case ERROR:
                     // Handle error response
-                    Log.d("user",response.getError());
+                    Log.d("user", response.getError());
 
                     break;
 
                 // Most likely auth flow was cancelled
                 default:
                     // Handle other cases
-                    Log.d("user","error");
+                    Log.d("user", "error");
             }
         }
 
     }
 
-    public void getPlaylists(){
+    public void getPlaylists() {
         spotify.getMyPlaylists(new Callback<Pager<PlaylistSimple>>() {
             @Override
             public void success(Pager<PlaylistSimple> playlistSimplePager, Response response) {
                 userPlaylists.addAll(playlistSimplePager.items);
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-
-            }
-        });
-
-    }
-
-    public static ArrayList<String> getArtistsFromPlaylist(String userId,String playlistId){
-        final ArrayList<String> artists = new ArrayList<>();
-        spotify.getPlaylist(userId, playlistId, new Callback<Playlist>() {
-            @Override
-            public void success(Playlist playlist, Response response) {
-                List<PlaylistTrack> tracks = playlist.tracks.items;
-                for(PlaylistTrack track: tracks) {
-                    List<ArtistSimple> trackArtists = track.track.artists;
-                    if(trackArtists.size() > 1) {
-                        artists.add(trackArtists.get(1).name);
-                    }else{
-                        artists.add(trackArtists.get(0).name);
-                    }
+                for(PlaylistSimple simple: userPlaylists){
+                    getArtistsFromPlaylist(simple.owner.id,simple.id);
                 }
             }
 
@@ -163,20 +148,51 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        return artists;
+
+    }
+
+
+    public void getArtistsFromPlaylist(String userId, final String playlistId) {
+        spotify.getPlaylist(userId, playlistId, new Callback<Playlist>() {
+            @Override
+            public void success(Playlist playlist, Response response) {
+
+                List<PlaylistTrack> tracks = playlist.tracks.items;
+                String artists = "";
+                int count = 0;
+                ArrayList<String> repeats = new ArrayList<>();
+                for(PlaylistTrack track : tracks){
+                    if(count < 3) {
+                        if(!repeats.contains(track.track.artists.get(0).name)){
+                            repeats.add(track.track.artists.get(0).name);
+                            artists += track.track.artists.get(0).name + " , ";
+                            count++;
+                        }
+                    }else{
+                        break;
+                    }
+                }
+                PlaylistToArtists.put(playlistId,artists);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+            }
+        });
     }
 
     //Call playlist tracks and "Wait" for playlistSongs variable to be populated before using
     //Call playlistSongs static variable from other classes by calling MainActivity.playlistSongs
 
-    public static ArrayList<PlaylistTrack> getPlaylistTracks(String userId, String playlistId){
-        if(playlistSongs.size() > 0){
+    public static ArrayList<PlaylistTrack> getPlaylistTracks(String userId, String playlistId) {
+        if (playlistSongs.size() > 0) {
             playlistSongs.clear();
         }
 
         spotify.getPlaylistTracks(userId, playlistId, new Callback<Pager<PlaylistTrack>>() {
             @Override
             public void success(Pager<PlaylistTrack> playlistTrackPager, Response response) {
+
                 playlistSongs.addAll(playlistTrackPager.items);
             }
 
@@ -189,21 +205,24 @@ public class MainActivity extends AppCompatActivity {
         return tracks;
     }
 
-    private void initImageBitmaps(ArrayList<PlaylistSimple> userPlaylist, ArrayList<PlaylistTrack> playlistSongs){
+    private void initImageBitmaps(ArrayList<PlaylistSimple> userPlaylist, ArrayList<PlaylistTrack> playlistSongs) {
         Log.d("user", Integer.toString(userPlaylist.size()));
-        for(int i = 0; i < userPlaylist.size(); i++){
-            mImageUrls.add(userPlaylist.get(i).images.get(0).url);
+        for (int i = 0; i < userPlaylist.size() ; i++) {
+            if (userPlaylist.get(i).images.size() > 0){
+                mImageUrls.add(userPlaylist.get(i).images.get(0).url);
+            }else{
+                mImageUrls.add(null);
+            }
             mPlaylistName.add(userPlaylist.get(i).name);
             //THIS IS THE CORRECT LINE THAT SHOULD BE USED TO POPULATE ARTIST NAMES
-            //mNames.add(getArtistsFromPlaylist(userPlaylist.get(i).owner.id, userPlaylist.get(i).id).get(0));
-            mNames.add("Populate Names");
-            i++;
+            mNames.add(PlaylistToArtists.get(userPlaylist.get(i).id));
         }
+
 
         initRecyclerView();
     }
 
-    private void initRecyclerView(){
+    private void initRecyclerView() {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(mPlaylistName, mNames, mImageUrls, this);
         recyclerView.setAdapter(adapter);
@@ -212,4 +231,36 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+/*
+    public  static Track swapifySong(final Track original){
+        String albumId = original.album.id;
+        //check if album has another song available
+        final ArrayList<Track> result = new ArrayList<>();
+        spotify.getAlbumTracks(albumId, new Callback<Pager<Track>>() {
+            @Override
+            public void success(Pager<Track> trackPager, Response response) {
+                List<Track> tracks = trackPager.items;
+                for(Track track : tracks){
+                    if(track.id != original.id){
+                        result.add(track);                   }
+                }
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+        if(result.size() > 0){
+            return result.get(0);
+        }
+        else{
+            //find another song from the same artist
+
+        }
+    }
+    */
+
 }
+
